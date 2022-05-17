ef76a19ff17a586a97039459a9e6165ef791c5a3
rojae <rojae@kakao.com>
2022-05-18 01:46:01 +0900

    Initial commit

---
+++ src/main/java/com/rdelgatte/pitest/RangeValidator.java
@@ -0,0 +1,9 @@
+package com.rdelgatte.pitest;
+
+class RangeValidator {
+
+  boolean isValid(int input) {
+    return input > 0 && input <= 100;
+  }
+  int sum(int a, int b) { return a + b; };
+}
---
+++ src/test/java/com/rdelgatte/pitest/RangeValidatorTest.java
@@ -0,0 +1,53 @@
+package com.rdelgatte.pitest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RangeValidatorTest {
+
+  private RangeValidator cut;
+
+  @BeforeEach
+  void setUp() {
+    cut = new RangeValidator();
+  }
+
+  /**
+   * {@link RangeValidator#isValid(int)}
+   */
+
+  @Test
+  @DisplayName("Should return true given 50")
+  void fifty_isValid_returnsTrue() {
+    assertThat(cut.isValid(50)).isTrue();
+  }
+
+  @Test
+  @DisplayName("Should return false given 200")
+  void twoHundred_isValid_returnsFalse() {
+    assertThat(cut.isValid(200)).isFalse();
+  }
+
+  @Test
+  @DisplayName("Should return true given 100")
+  void hundred_isValid_returnsTrue() {
+    assertThat(cut.isValid(100)).isTrue();
+  }
+
+  @Test
+  @DisplayName("Should return false given 0")
+  void zero_isValid_returnsFalse() {
+    assertThat(cut.isValid(0)).isFalse();
+  }
+
+  @Test
+  @DisplayName("Sum Two Numbers Given 0, 0 ")
+  void zero_zero_sum_Test()  { assertThat(cut.sum(1,10 )).isEqualTo(11); }
+
+  @Test
+  @DisplayName("Sum Two Numbers Given 1, 0")
+  void one_zero_sum_Test()  { assertThat(cut.sum(1,1 )).isEqualTo(2); }
+}
